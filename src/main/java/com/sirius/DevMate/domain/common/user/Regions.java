8bed93112366f2bee840245508bfaf4e24d74e9f
Sirius147 <lsimma7@naver.com>
2025-09-11 18:42:24 +0900

    일부 변경 사항 저장

--- src/main/java/com/sirius/DevMate/domain/common/user/Regions.java
+++ src/main/java/com/sirius/DevMate/domain/common/user/Regions.java
@@ -3,8 +3,7 @@ package com.sirius.DevMate.domain.common.user;
 public enum Regions {
     SEOUL, GYEONGGIDO,
     GANGWONDO,
-    CHUNGCHEONGBUKDO,
-    CHUNGCHEONGNAMDO,
+    CHUNGCHEONGDO,
     JEOLLANAMDO,
     JEOLLABUKDO,
     GYEONGSANGNAMDO,
